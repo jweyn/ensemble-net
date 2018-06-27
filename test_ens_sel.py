fe248c2f4cd1ed34ea6fa32337c36174c06cf102
jweyn <jweyn@uw.edu>
2018-06-27 12:38:24 -0700

    Skeleton for ensemble_selection test

--- test_ens_sel.py
+++ test_ens_sel.py
@@ -13,23 +13,22 @@ NCAR ensemble forecast at individual stations.
 from ensemble_net.data_tools import NCARArray, MesoWest
 from ensemble_net.util import date_to_meso_date
 from ensemble_net.verify import ae_meso
-from ensemble_net.plot import plot_basemap
-from ensemble_net.ensemble_selection.preprocessing import predictors_from_ae_meso, predictors_from_ensemble
-import matplotlib.pyplot as plt
+from ensemble_net.ensemble_selection import preprocessing, model
 import pandas as pd
-import numpy as np
+import time
 import xarray as xr
 from datetime import datetime, timedelta
 
 
 # Ensemble data parameters
 start_init_date = datetime(2016, 4, 1)
-end_init_date = datetime(2016, 4, 4)
+end_init_date = datetime(2016, 4, 10)
 pd_date_range = pd.date_range(start=start_init_date, end=end_init_date, freq='D')
 init_dates = list(pd_date_range.to_pydatetime())
 forecast_hours = list(range(0, 25, 12))
 members = list(range(1, 11))
-variables = ('TMP2',)
+forecast_variables = ('TMP2', 'DPT2', 'MSLP', 'CAPE')
+verification_variables = ('TMP2', 'DPT2', 'MSLP')
 
 # Subset with grid parameters
 lat_0 = 25.
@@ -63,10 +62,72 @@ error_ds = xr.open_dataset('extras/mesowest-error-201604.nc')
 
 
 # Generate the predictors and targets
-forecast_predictors = predictors_from_ensemble(ensemble, (lon_0, lon_1), (lat_0, lat_1), variables=variables,
-                                               convolution=100, convolution_step=50, verbose=True)
-error_predictors = predictors_from_ae_meso(error_ds, ensemble, (lon_0, lon_1), (lat_0, lat_1), variables=variables,
-                                           convolution=100, convolution_step=50, convolution_agg='mse', verbose=True)
+forecast_predictors = preprocessing.predictors_from_ensemble(ensemble, (lon_0, lon_1), (lat_0, lat_1),
+                                                             variables=forecast_variables,
+                                                             convolution=100, convolution_step=50, verbose=True)
+error_predictors = preprocessing.predictors_from_ae_meso(error_ds, ensemble, (lon_0, lon_1), (lat_0, lat_1),
+                                                         variables=verification_variables,
+                                                         convolution=100, convolution_step=50, convolution_agg='mse',
+                                                         verbose=True)
+# Targets are the 3rd time step in the error predictors
+if forecast_predictors.shape[0] < error_predictors.shape[0]:
+    error_predictors = error_predictors[:forecast_predictors.shape[0]]
+ae_predictors, ae_targets = (1. * error_predictors[:, :, :, :2, :], 1. * error_predictors[:, :, :, [2], :])
 
 
+# Reshape the predictors and targets
+forecast_predictors, fpi = preprocessing.convert_ensemble_predictors_to_samples(forecast_predictors, convolved=True)
+ae_predictors, epi = preprocessing.convert_ae_meso_predictors_to_samples(ae_predictors, convolved=True)
+ae_targets, eti = preprocessing.convert_ae_meso_predictors_to_samples(ae_targets, convolved=True)
+combined_predictors = preprocessing.combine_predictors(forecast_predictors, ae_predictors)
+
+
+# Write pickle files
+predictor_file = 'extras/ens_sel_predictors_201604'
+preprocessing.train_data_to_pickle(predictor_file, combined_predictors, ae_targets)
+# combined_predictors, error_targets = preprocessing.train_data_from_pickle(predictor_file)
+
+
+# Remove samples with NaN
+predictors, targets = preprocessing.delete_nan_samples(combined_predictors, ae_targets)
+
+
+# Split into train and test sets. Either random subset or last 20%.
+# p_train, p_test, t_train, t_test = train_test_split(predictors, targets, test_size=0.2)
+num_samples = combined_predictors.shape[0]
+split = -1*num_samples//5
+p_train = combined_predictors[:split]
+p_test = combined_predictors[split:]
+t_train = ae_targets[:split]
+t_test = ae_targets[split:]
+
+
+# Build an ensemble selection model
+selector = model.EnsembleSelector()
+layers = (
+    # ('Conv2D', (64,), {
+    #     'kernel_size': (3, 3),
+    #     'activation': 'relu',
+    #     'input_shape': input_shape
+    # }),
+    ('Dense', (512,), {
+        'activation': 'relu'
+    }),
+    ('Dropout', (0.25,), {}),
+    ('Dense', (128,), {
+        'activation': 'linear'
+    })
+)
+selector.build_model(layers=layers, loss='mse', optimizer='adam', metrics=['mae'])
+
+
+# Train an evaluate the model
+start_time = time.time()
+selector.fit(p_train, t_train, batch_size=64, epochs=6, verbose=1, validation_data=(p_test, t_test))
+end_time = time.time()
+
+score = selector.evaluate(p_test, t_test, verbose=0)
+print("\nTrain time -- %s seconds --" % (end_time - start_time))
+print('Test loss:', score[0])
+print('Test mean absolute error:', score[1])
 
